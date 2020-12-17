d6db2729581badd84797f5d7ac70dcaa8bb7ce6e
Ajinkya <42488697+nanawareajinkya@users.noreply.github.com>
2020-12-17 12:13:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
     static defaultProps = {
         displayType: 'Top',
-        username: 'Reddit-User'
+        username: 'React-Reddit-User'
     }
 
     render() {
